2129b7fe4d351aa76ce540e09eda9a3701f92ef2
PhantHive <phantomhive72@yahoo.fr>
2021-10-04 16:33:12 +0200

    Title: Stopwords remove function
    Desc: French stopwords are removed from any text during tokenization

--- tokenizer.py
+++ tokenizer.py
@@ -1,3 +1,6 @@
+import json
+
+
 class TOKENIZE:
 
     def __init__(self, text):
@@ -7,6 +10,10 @@ class TOKENIZE:
         self.new_text = ""
         self.all_words = []
         self.tokenize_matrix = []
+
+        stopwords = open('./src/stopwords/stop_words_french.json', 'r')
+        self.stopwords = json.load(stopwords)
+
         self.text_to_sentences()
 
     def text_to_sentences(self):
@@ -21,6 +28,10 @@ class TOKENIZE:
 
         self.tokenize()
 
+    def remove_stopwords(self):
+        temp_list = [word for word in self.all_words if word.lower() not in self.stopwords]
+        return temp_list
+
     def tokenize(self):
 
         words_occ = {}
@@ -28,6 +39,8 @@ class TOKENIZE:
         for sentence in self.all_sentences:
             words = sentence.strip(" ").split(" ")
             self.all_words += words
+
+        self.all_words = self.remove_stopwords()
 
         for sentence in self.all_sentences:
             for e in self.all_words:
@@ -48,8 +61,37 @@ class TOKENIZE:
 
 # tries ===============================
 
-texte = 'I like to eat noodles. But I prefer girls with big boobies! ' \
-        'This is a stupid text. Isn\'t it? They slither while they pass, they slip away across the universe.'
+texte = 'En mon cœur n\'est point escrite'\
+'La rose ny autre fleur,'\
+'C\'est toy, blanche Marguerite,'\
+'Par qui j\'ay cette couleur.'\
+'N\'es-tu celle dont les yeux'\
+'Ont surpris'\
+'Par un regard gracieux'\
+'Mes esprits ?'\
+'Puis que ta sœur de haut pris,'\
+'Ta sœur, pucelle d\'élite,'\
+'N\'est cause de ma douleur,'\
+'C\'est donc par toy, Marguerite'\
+'Que j\'ay pris ceste couleur.'\
+'Ma couleur palle nasquit,'\
+'Quand mon cœur'\
+'Pour maistresse te requit ;'\
+'Mais rigueur'\
+'D\'une amoureuse langueur'\
+'Soudain paya mon mérite,'\
+'Me donnant ceste pâleur'\
+'Pour t\'aimer trop, Marguerite,'\
+'Et ta vermeille couleur.'\
+'Quel charme pourroit casser'\
+'Mon ennuy'\
+'Et ma couleur effacer'\
+'Avec luy ?'\
+'De l\'amour que tant je suy'\
+'La jouissance subite'\
+'Seule osteroit le malheur'\
+'Que me donna Marguerite,'\
+'Par qui j\'ay cette couleur.'\
 
 test = TOKENIZE(texte)
 res, res2, res3 = test.show_sentences()
