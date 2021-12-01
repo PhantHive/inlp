edd3e9173490ef87e7656f5f429f1ab3e3d7acf1
Restyled.io <commits@restyled.io>
2021-12-01 21:31:26 +0000

    Restyled by black

--- tokenizer.py
+++ tokenizer.py
@@ -2,7 +2,6 @@ import json
 
 
 class TOKENIZE:
-
     def __init__(self, text):
         self.text = text
         self.all_sentences = []
@@ -11,12 +10,12 @@ class TOKENIZE:
         self.all_words = []
         self.tokenize_matrix = []
 
-        stopwords = open('assets/stopwords/stop_words_french.json', 'r')
+        stopwords = open("assets/stopwords/stop_words_french.json", "r")
         self.stopwords = json.load(stopwords)
         self.text_to_sentences()
 
     def text_to_sentences(self):
-        end_sentence = ['!', '?', '.', ':', ';']
+        end_sentence = ["!", "?", ".", ":", ";"]
 
         for let in self.text:
             if let in end_sentence:
@@ -34,7 +33,13 @@ class TOKENIZE:
                         if i.isupper() and word.index(i) > 0:
                             z = self.sentence.index(word)
                             self.sentence[z] = word.split(i, 1)[0]
-                            self.sentence.insert(z + 1, i + word.split(i, )[1])
+                            self.sentence.insert(
+                                z + 1,
+                                i
+                                + word.split(
+                                    i,
+                                )[1],
+                            )
 
                 self.new_text = self.text.split(let, 1)[1]
                 self.all_sentences.append(self.sentence)
@@ -50,7 +55,8 @@ class TOKENIZE:
             self.all_words += word
 
         self.all_words = [
-            word for word in self.all_words if word.lower() not in self.stopwords]
+            word for word in self.all_words if word.lower() not in self.stopwords
+        ]
 
         for sentence in self.all_sentences:
             for i in self.all_words:
@@ -68,41 +74,43 @@ class TOKENIZE:
     def show_sentences(self):
         return self.all_sentences, self.tokenize_matrix, self.all_words
 
+
 # tries ===============================
 
 
-texte = 'En mon cœur n\'est point escrite'\
-    'La rose ny autre fleur,'\
-    'C\'est toy, blanche Marguerite,'\
-    'Par qui j\'ay cette couleur.'\
-    'N\'es-tu celle dont les yeux'\
-    'Ont surpris'\
-    'Par un regard gracieux'\
-    'Mes esprits ?'\
-    'Puis que ta sœur de haut pris,'\
-    'Ta sœur, pucelle d\'élite,'\
-    'N\'est cause de ma douleur,'\
-    'C\'est donc par toy, Marguerite'\
-    'Que j\'ay pris ceste couleur.'\
-    'Ma couleur palle nasquit,'\
-    'Quand mon cœur'\
-    'Pour maistresse te requit ;'\
-    'Mais rigueur'\
-    'D\'une amoureuse langueur'\
-    'Soudain paya mon mérite,'\
-    'Me donnant ceste pâleur'\
-    'Pour t\'aimer trop, Marguerite,'\
-    'Et ta vermeille couleur.'\
-    'Quel charme pourroit casser'\
-    'Mon ennuy'\
-    'Et ma couleur effacer'\
-    'Avec luy ?'\
-    'De l\'amour que tant je suy'\
-    'La jouissance subite'\
-    'Seule osteroit le malheur'\
-    'Que me donna Marguerite,'\
-    'Par qui j\'ay cette couleur.'\
-
+texte = (
+    "En mon cœur n'est point escrite"
+    "La rose ny autre fleur,"
+    "C'est toy, blanche Marguerite,"
+    "Par qui j'ay cette couleur."
+    "N'es-tu celle dont les yeux"
+    "Ont surpris"
+    "Par un regard gracieux"
+    "Mes esprits ?"
+    "Puis que ta sœur de haut pris,"
+    "Ta sœur, pucelle d'élite,"
+    "N'est cause de ma douleur,"
+    "C'est donc par toy, Marguerite"
+    "Que j'ay pris ceste couleur."
+    "Ma couleur palle nasquit,"
+    "Quand mon cœur"
+    "Pour maistresse te requit ;"
+    "Mais rigueur"
+    "D'une amoureuse langueur"
+    "Soudain paya mon mérite,"
+    "Me donnant ceste pâleur"
+    "Pour t'aimer trop, Marguerite,"
+    "Et ta vermeille couleur."
+    "Quel charme pourroit casser"
+    "Mon ennuy"
+    "Et ma couleur effacer"
+    "Avec luy ?"
+    "De l'amour que tant je suy"
+    "La jouissance subite"
+    "Seule osteroit le malheur"
+    "Que me donna Marguerite,"
+    "Par qui j'ay cette couleur."
+)
 test = TOKENIZE(texte)
 res, res2, res3 = test.show_sentences()
 
