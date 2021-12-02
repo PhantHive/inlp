1249d3b99f9144af30e09b60f27d8ebfd9f9e136
Restyled.io <commits@restyled.io>
2021-12-02 14:41:38 +0000

    Restyled by yapf

--- tokenizer.py
+++ tokenizer.py
@@ -34,10 +34,7 @@ class TOKENIZE:
                             self.sentence[z] = word.split(i, 1)[0]
                             self.sentence.insert(
                                 z + 1,
-                                i
-                                + word.split(
-                                    i,
-                                )[1],
+                                i + word.split(i, )[1],
                             )
                 for word in self.sentence:
                     if "'" in word and word.index("'") > 0:
@@ -45,19 +42,14 @@ class TOKENIZE:
                         self.sentence[z] = word.split("'", 1)[0]
                         self.sentence.insert(
                             z + 1,
-                            word.split(
-                                "'",
-                            )[1],
+                            word.split("'", )[1],
                         )
                     if word == "":
                         self.sentence.remove(word)
 
                         self.sentence.insert(
                             z + 1,
-                            i
-                            + word.split(
-                                i,
-                            )[1],
+                            i + word.split(i, )[1],
                         )
 
                 self.new_text = self.text.split(let, 1)[1]
@@ -76,7 +68,8 @@ class TOKENIZE:
             self.all_words += word
 
         self.all_words = [
-            word for word in self.all_words if word.lower() not in self.stopwords
+            word for word in self.all_words
+            if word.lower() not in self.stopwords
         ]
 
         for sentence in self.all_sentences:
@@ -100,39 +93,37 @@ class TOKENIZE:
 
 # tries ===============================
 
-texte = (
-    "En mon cœur n'est point escrite"
-    "La rose ny autre fleur,"
-    "C'est toy, blanche Marguerite,"
-    "Par qui j'ay cette couleur."
-    "N'es-tu celle dont les yeux"
-    "Ont surpris"
-    "Par un regard gracieux"
-    "Mes esprits ?"
-    "Puis que ta sœur de haut pris,"
-    "Ta sœur, pucelle d'élite,"
-    "N'est cause de ma douleur,"
-    "C'est donc par toy, Marguerite"
-    "Que j'ay pris ceste couleur."
-    "Ma couleur palle nasquit,"
-    "Quand mon cœur"
-    "Pour maistresse te requit ;"
-    "Mais rigueur"
-    "D'une amoureuse langueur"
-    "Soudain paya mon mérite,"
-    "Me donnant ceste pâleur"
-    "Pour t'aimer trop, Marguerite,"
-    "Et ta vermeille couleur."
-    "Quel charme pourroit casser"
-    "Mon ennuy"
-    "Et ma couleur effacer"
-    "Avec luy ?"
-    "De l'amour que tant je suy"
-    "La jouissance subite"
-    "Seule osteroit le malheur"
-    "Que me donna Marguerite,"
-    "Par qui j'ay cette couleur."
-)
+texte = ("En mon cœur n'est point escrite"
+         "La rose ny autre fleur,"
+         "C'est toy, blanche Marguerite,"
+         "Par qui j'ay cette couleur."
+         "N'es-tu celle dont les yeux"
+         "Ont surpris"
+         "Par un regard gracieux"
+         "Mes esprits ?"
+         "Puis que ta sœur de haut pris,"
+         "Ta sœur, pucelle d'élite,"
+         "N'est cause de ma douleur,"
+         "C'est donc par toy, Marguerite"
+         "Que j'ay pris ceste couleur."
+         "Ma couleur palle nasquit,"
+         "Quand mon cœur"
+         "Pour maistresse te requit ;"
+         "Mais rigueur"
+         "D'une amoureuse langueur"
+         "Soudain paya mon mérite,"
+         "Me donnant ceste pâleur"
+         "Pour t'aimer trop, Marguerite,"
+         "Et ta vermeille couleur."
+         "Quel charme pourroit casser"
+         "Mon ennuy"
+         "Et ma couleur effacer"
+         "Avec luy ?"
+         "De l'amour que tant je suy"
+         "La jouissance subite"
+         "Seule osteroit le malheur"
+         "Que me donna Marguerite,"
+         "Par qui j'ay cette couleur.")
 test = TOKENIZE(texte)
 res, res2, res3 = test.show_sentences()
 
