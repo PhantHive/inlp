670ca6270ed1afc733502e0d3dc29f54412b9469
PhantHive <io.apocs@outlook.fr>
2022-02-03 01:45:47 +0100

    Make Lemmer

--- src/lemmer.py
+++ src/lemmer.py
@@ -7,19 +7,34 @@ class Lemmer:
 
         self.w = w
 
-        self.lemmer_path = os.path.dirname('../assets/dict/lemmatization-fr.txt')
-        #assert os.path.isfile(self.lemmer_path)
-        self.lemmer_file = open(self.lemmer_path, "r").read()
+        self.lemmer_path = '../assets/dict/lemmatization-fr.txt'
+        self.lemmer_path_2 = '../assets/dict/lemmatization-fr-2.txt'
+
+        self.lemmer_file = open(self.lemmer_path, "r")
+        self.lemmer_file_2 = open(self.lemmer_path_2, "r")
 
 
     def lemm(self):
 
         for row in self.lemmer_file:
 
-            base_word = row.split(" ")[0]
-            print(base_word)
-            print(self.w)
+            base_word = row.split()[0]
+
             if self.w == base_word:
+                print(base_word)
+                print(self.w)
+                print("match")
+                return
+
+
+        for row in self.lemmer_file_2:
+
+
+            base_word = row.split()[0]
+
+            if self.w == base_word:
+                print(base_word)
+                print(self.w)
                 print("match")
                 return
 
