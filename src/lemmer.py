48584ccc4052e6b7b65265cbbe0cac36adef5564
PhantHive <io.apocs@outlook.fr>
2022-02-03 02:08:54 +0100

    Lemmer: update txt encoding utf8

--- src/lemmer.py
+++ src/lemmer.py
@@ -1,18 +1,18 @@
-import os
+import unidecode
+
 
 class Lemmer:
-
 
     def __init__(self, w):
 
         self.w = w
+        print(self.w)
 
         self.lemmer_path = '../assets/dict/lemmatization-fr.txt'
         self.lemmer_path_2 = '../assets/dict/lemmatization-fr-2.txt'
 
-        self.lemmer_file = open(self.lemmer_path, "r")
-        self.lemmer_file_2 = open(self.lemmer_path_2, "r")
-
+        self.lemmer_file = open(self.lemmer_path, "r", encoding="utf8")
+        self.lemmer_file_2 = open(self.lemmer_path_2, "r", encoding="utf8")
 
     def lemm(self):
 
@@ -21,28 +21,22 @@ class Lemmer:
             lem_word = row.split()[1]
 
             if self.w == lem_word:
-
                 base_word = row.split()[0]
                 return base_word
 
-
         for row in self.lemmer_file_2:
-
 
             lem_word = row.split()[1]
 
             if self.w == lem_word:
-
                 base_word = row.split()[0]
                 return base_word
 
 
-
 if __name__ == '__main__':
-
-    lemmer = Lemmer("extorquions")
+    
+    lemmer = Lemmer("éjaculât")
 
     bw = lemmer.lemm()
 
-    print(bw) # print base word :)
-
+    print(bw)  # print base word :)
